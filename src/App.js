792694cf964a2eadfd441d7bb0b1e935a3cd7b8a
Harsh Gandhi <harshgandhi@Harshs-MacBook-Air.local>
2018-05-15 00:59:02 +0530

    Create Chat functionality implemented using Relay

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
 import React, { Component } from 'react';
-import { graphql, compose } from 'react-apollo';
-import gql from 'graphql-tag';
 
 import ChatListPage from './components/ChatListPage';
+import CreateChat from './components/CreateChat';
 import './App.css';
 
 class App extends Component {
-  state = {
-    from: 'Anonymous',
-    content: ''
-  };
+  
   componentDidMount() {
     // const from = window.prompt('Enter Username');
     // from && this.setState({ from });
     // this.subscribeToNewChats();
   }
   
-  createChat = e => {
-    console.log('Create chat called');
-  };
   render() {
     return (
       <div className="">
         <div className="container">
           <h2>Chats</h2>
           <ChatListPage />
+          <CreateChat />
         </div>
-        <div className="input-wrapper">
-              <input
-                value={this.state.content}
-                onChange={e => this.setState({ content: e.target.value })}
-                type="text"
-                placeholder="Start typing..."
-                onKeyPress={this.createChat}
-              />
-          </div>
       </div>
     );
   }
